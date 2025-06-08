d5348fb2eab6696cd1d25c7c93432851baaab29a
theahmed619 <theahmed619@gmail.com>
2025-06-08 21:22:49 +0530

    add emp code

---
+++ Employee-Management-System/Employee-Management-System/WebForm1.aspx
@@ -0,0 +1,86 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Employee_Management_System.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>EMP</title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container ">
+            &nbsp;&nbsp;
+            <asp:Label ID="lblId" runat="server" Text="ID:"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <input id="txtId" type="text" runat="server" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblFname" runat="server" Text="First Name:"></asp:Label>
+            <asp:TextBox ID="txtFname" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblLname" runat="server" Text="Last Name:"></asp:Label>
+            <asp:TextBox ID="txtLname" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lblGender" runat="server" Text="Gender:"></asp:Label>
+            <asp:DropDownList ID="ddlGender" runat="server">
+            </asp:DropDownList>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCity" runat="server" Text="City:"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlCity" runat="server">
+            </asp:DropDownList>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblActive" runat="server" Text="isActive:"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:DropDownList ID="ddlActive" runat="server">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Label ID="lblTotal" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            <br />
+            <br />
+            <div >
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnAdd" runat="server" Text="ADD" OnClick="btnAdd_Click" />
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnSelect" runat="server" OnClick="btnSelect_Click" Text="Select" />
+                &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+                &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                <br />
+            </div>
+
+        </div>
+        <div class="d-flex justify-content-center p-4">
+
+            <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" class="text-center" Width="1350" CellPadding="4" ForeColor="#333333" GridLines="None">
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView>
+        </div>
+
+
+
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ Employee-Management-System/Employee-Management-System/WebForm1.aspx.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee_Management_System
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //Connection String
+        string conStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) // Prevent reloading on every postback
+            {
+                populateCities();
+                isActive();
+                LoadGrid();
+                Gender();
+
+
+            }
+            totalEmployees();
+        }
+
+        private void Gender()
+        {
+            List<String> genders = new List<string>
+            {
+                "Male","Female"
+            };
+            ddlGender.DataSource = genders;
+            ddlGender.DataBind();
+            ddlGender.Items.Insert(0,"Select Gender");
+
+        }
+
+        private void isActive()
+        {
+            List<string> actives = new List<string>();
+            actives.Add("Active");
+            actives.Add("Not");
+            ddlActive.DataSource = actives;
+            ddlActive.DataBind();
+            ddlActive.Items.Insert(0, "Select "); // Optional
+        }
+
+        private void totalEmployees()
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                string query = "select count(*) from Employees";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                Int32 count = Convert.ToInt32(cmd.ExecuteScalar());
+                if (count > 0)
+                {
+                    lblTotal.Text = "Total Employees is " + count;
+                }
+                else
+                {
+                    lblTotal.Text = "No Employee Hired yet";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        
+        }
+        private void populateCities()
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                List<string> listOfCities = new List<string>();
+                string query = "select * from City";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listOfCities.Add(reader.GetString(1));
+                }
+                ddlCity.DataSource = listOfCities;
+                ddlCity.DataBind();
+                ddlCity.Items.Insert(0, "Select City"); // Optional
+
+
+            }
+            catch(Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                if (!string.IsNullOrEmpty(txtId.Value))
+                {
+                    string query = "Select * from Employees where Id=@ID";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@ID", txtId.Value);
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        txtFname.Text = !reader.IsDBNull(1) ? reader.GetString(1) : "";
+                        txtLname.Text = !reader.IsDBNull(2) ? reader.GetString(2) : "";
+                        ddlCity.SelectedValue = !reader.IsDBNull(3) ? reader.GetString(3) : "";
+                        ddlActive.SelectedValue = !reader.IsDBNull(4) ? reader.GetString(4) : "";
+                        ddlGender.SelectedValue = !reader.IsDBNull(5) ? reader.GetString(5) : "";
+
+                    }
+                }
+                else
+                {
+                    lblMsg.Text = "Enter Emplyee ID";
+                }
+
+            }catch(Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conStr);
+
+            try
+            {
+                string query = @"insert into employees(FirstName, LastName, City, Status, Gender) values(@FirstName, @LastName, @City, @Status, @Gender) ";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@FirstName", txtFname.Text);
+                cmd.Parameters.AddWithValue("@LastName", txtLname.Text);
+                cmd.Parameters.AddWithValue("@City", ddlCity.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@Status", ddlActive.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedItem.Value);
+                con.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    lblMsg.Text = "Record Inserted Successfully";
+                    totalEmployees();
+                    LoadGrid();
+
+                }
+                else
+                {
+                    lblMsg.Text = "Insertion Failed";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conStr);
+
+            try
+            {
+                string query = "update employees set FirstName=@Fname, LastName=@Lname, City=@City,Status=@Status, Gender=@Gender where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Fname", txtFname.Text);
+                cmd.Parameters.AddWithValue("@Lname", txtLname.Text);
+                cmd.Parameters.AddWithValue("@City", ddlCity.SelectedValue);
+                cmd.Parameters.AddWithValue("@Status", ddlActive.Text);
+                cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                cmd.Parameters.AddWithValue("@Id", txtId.Value);
+                con.Open();
+                int row = cmd.ExecuteNonQuery();
+                
+                    if (row > 0)
+                    {
+                        lblMsg.Text = "Updation Successfully";
+                    LoadGrid();
+                }
+                else
+                {
+                    lblMsg.Text = "Updation Failed";
+                }
+
+            }
+            catch(Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conStr);
+
+            try
+            {
+                string query = "delete from employees where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Id", txtId.Value);
+                con.Open();
+                int row = cmd.ExecuteNonQuery();
+
+                if (row > 0)
+                {
+                    lblMsg.Text = "Deletion Successfully";
+                    totalEmployees();
+                    LoadGrid();
+                }
+                else
+                {
+                    lblMsg.Text = "Deletion Failed";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    
+
+        private void LoadGrid()
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                string query = "SELECT * FROM employees";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conStr);
+            try
+            {
+                string query = "select * from employees";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                con.Open();
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+
+
+            }
+            catch(Exception ex)
+            {
+                lblMsg.Text = ex.Message;
+
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
